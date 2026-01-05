e0a9ab1474d69899d03e619dc4b44b2d01a633e1
ddmuzyk <domino2k1@gmail.com>
2026-01-05 17:09:22 +0100

    fix

--- server/controllers/availability.py
+++ server/controllers/availability.py
@@ -61,13 +61,13 @@ def get_availabilities_by_specialization_and_date():
                     "availability_id": a['availability_id'],
                     "start_time": str(a['start_time']),
                     "end_time": str(a['end_time']),
+                    "is_available": a['is_available'],
                     "doctor": {
                         "doctor_id": a['doctor_id'],
                         "first_name": a['first_name'],
                         "last_name": a['last_name'],
                         "specialization": a['specialization'],
                         "license_number": a['license_number'],
-                        "is_available": a['is_available']
                     }
                 }
                 for a in availabilities
